77b77f82f7d1db4c88425b7f6e4a2d470b90cacb
Duc Nguyen <cumbee2k2@gmail.com>
2025-09-26 09:49:23 +0200

    feat: crud function interface for app data models

---
+++ src/interface/crud/university/__init__.py
@@ -0,0 +1,24 @@
+# University-related CRUD operations
+
+# Base university CRUD operations
+from .base import *
+
+# Specific university CRUD operations
+from .uniCampus import *
+from .uniCareer import *
+from .uniFinance import *
+from .uniPeople import *
+from .uniProgram import *
+from .uniResearch import *
+from .uniScholarship import *
+
+__all__ = [
+    # University CRUD operations will be added when implemented
+    # Campus CRUD operations will be added when implemented
+    # Career CRUD operations will be added when implemented
+    # Finance CRUD operations will be added when implemented
+    # People CRUD operations will be added when implemented
+    # Program CRUD operations will be added when implemented
+    # Research CRUD operations will be added when implemented
+    # Scholarship CRUD operations will be added when implemented
+]
