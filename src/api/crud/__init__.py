ee2a01a0d28116314bde565080d0c6f06975682b
Duc Nguyen <cumbee2k2@gmail.com>
2025-09-29 03:24:41 +0200

    chore: combine all api endpoints

--- src/api/crud/__init__.py
+++ src/api/crud/__init__.py
@@ -8,14 +8,16 @@ for different entities in the system.
 from fastapi import APIRouter
 from .user import user_router
 from .university import university_router
+from .scholarship import scholarship_router
 # from .plan import plan_router
 
 # Create the main CRUD router
 crud_router = APIRouter(tags=["CRUD Operations"])
 
 # Include sub-routers
-crud_router.include_router(user_router, tags=["User CRUD"])
-crud_router.include_router(university_router, tags=["University CRUD"])
+crud_router.include_router(user_router, tags=["CRUD User"])
+crud_router.include_router(university_router, tags=["CRUD University"])
+crud_router.include_router(scholarship_router, tags=["CRUD Scholarship"])
 # crud_router.include_router(plan_router, tags=["Plan CRUD"])
 
 __all__ = ["crud_router"]
